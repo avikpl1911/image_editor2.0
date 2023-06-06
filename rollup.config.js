8adc98831771d6e2d18e596e97fc367563b984ae
avikpl1911 <avikpl1911@gmail.com>
2023-06-06 17:27:25 +0530

    first

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+// rollup.config.js
+
+// Other Rollup configuration settings...
+
+export default {
+    // Other Rollup configuration settings...
+  
+    build: {
+      // Other build settings...
+  
+      rollupOptions: {
+        // Other Rollup options...
+  
+        external: [`wasm-lib`],
+  
+        // Other Rollup options...
+      },
+  
+      // Other build settings...
+    },
+  
+    // Other Rollup configuration settings...
+  };
+  
